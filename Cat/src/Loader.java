16560c7de0c3746c7f0e2517bc884c952ae52dfd
AlexRayn <alexraynwork@gmail.com>
2023-12-31 23:00:09 +0200

    added comments

--- Cat/src/Loader.java
+++ Cat/src/Loader.java
@@ -9,18 +9,23 @@ public class Loader
 
         Cat bob = new Cat();
         System.out.println("Боб " + bob.getStatus());
+
+
         bob.feed(6000.0);
         System.out.println("bob " + bob.getStatus());
 
 
+        //прекармливаем животное т.е countCats -1
         Cat alex = new Cat();
         alex.feed(100000.0);
-        alex.getStatus();
+        System.out.println("alex " + alex.getStatus());
 
         Cat murka = new Cat();
         System.out.println("вес мурки - " + murka.getWeight());
         murka.meow();
         System.out.println("вес мурки - " + murka.getWeight());
+
+        //создали цикл пока вес не будет равен 1000, происходит метод мяу уменьшающий вес на 1 грамм.
         while(murka.getWeight() >= 1000 ) {murka.meow();}
 
 
@@ -53,6 +58,8 @@ public class Loader
         System.out.println("===============================================");
         System.out.println("виталий - " + vitalii.getWeight());
         System.out.println("том - " + tom.getWeight());
+
+        //выводим в консоль количество живых кошек, используя метод countCats.
         System.out.println("количество живых кошек - " + Cat.countCats);
 
     }
