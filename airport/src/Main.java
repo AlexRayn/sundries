feb54e32686d68c19f1048f544ee7f90c90ebfa5
AlexRayn <alexraynwork@gmail.com>
2024-09-03 10:08:20 +0100

    repository synchronization

---
+++ airport/src/Main.java
@@ -0,0 +1,22 @@
+import com.skillbox.airport.Airport;
+
+import java.time.LocalDateTime;
+import java.util.Calendar;
+
+public class Main {
+    public static void main(String[] args) {
+        //Распечатайте с помощью библиотеки airport.jar время вылета
+        // и модели самолётов, вылетающих в ближайшие два часа.
+
+        //получаем экземпляр аэропорта
+        Airport airport = Airport.getInstance();
+
+        //создание класса Calendar
+        Calendar calendar = Calendar.getInstance();
+
+
+
+
+
+    }
+}
