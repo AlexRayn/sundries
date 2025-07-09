7601edf8a1cd6544764b71b308b1d7c1aba35ef0
AlexRayn <alexraynwork@gmail.com>
2025-07-09 05:27:54 +0300

    Print departure times for flights scheduled within the next two hours using the Airport library
    - Utilized the external library `airport` to access flight and terminal data.
    - Filtered outbound flights (`Flight.Type.DEPARTURE`) departing within the next two hours.
    - Printed the aircraft model and the scheduled departure time for each flight meeting the criteria.

--- airport/src/Main.java
+++ airport/src/Main.java
@@ -1,22 +1,42 @@
 import com.skillbox.airport.Airport;
+import com.skillbox.airport.Flight;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 import java.time.LocalDateTime;
 import java.util.Calendar;
 
 public class Main {
     public static void main(String[] args) {
-        //Распечатайте с помощью библиотеки airport.jar время вылета
-        // и модели самолётов, вылетающих в ближайшие два часа.
 
-        //получаем экземпляр аэропорта
+        //получает текущею дату
+        LocalDateTime now = LocalDateTime.now();
+        //время через два часа
+        LocalDateTime twoHoursFromNow = now.plusHours(2);
+
+        //создаем новый объект аэропорта
         Airport airport = Airport.getInstance();
 
-        //создание класса Calendar
-        Calendar calendar = Calendar.getInstance();
+        //Распечатайте с помощью библиотеки airport.jar время вылета и модели самолётов, вылетающих в ближайшие два часа.
 
-
-
-
-
+        //Получаем стрим из самолетов из терминалов
+        airport.getTerminals().stream()
+                // Получаем потоки рейсов из терминалов
+                .flatMap(terminal -> terminal.getFlights().stream())
+                // Фильтруем только исходящие рейсы
+                .filter(flight -> flight.getType() == Flight.Type.DEPARTURE)
+                // Фильтруем только те рейсы, которые вылетают в ближайшие 2 часа
+                .filter(flight -> {
+                    Date flightDate = flight.getDate();
+                    LocalDateTime flightDateTime = flightDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+                    return flightDateTime.isAfter(now) && flightDateTime.isBefore(twoHoursFromNow);
+                })
+                // Печатаем модель самолёта и время вылета
+                .forEach(flight -> {
+                    System.out.println("Модель самолёта: " + flight.getAircraft().getModel());
+                    System.out.println("Время вылета: " + flight.getDate());
+                });
     }
 }
