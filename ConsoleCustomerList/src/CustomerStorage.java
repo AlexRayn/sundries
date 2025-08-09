f1be89bc28b1f492c529d3f17c38194cf41bc4e8
AlexRayn <alexraynwork@gmail.com>
2025-08-09 09:00:24 +0300

    Added data validation to prevent input errors using throw and try-catch construct
    - Implemented email format validation (universal regular expression)
    - Fixed phone number validation, added format
    - Added user-friendly error messages for better interaction
    - Added protection against invalid commands in the addCustomer method

--- ConsoleCustomerList/src/CustomerStorage.java
+++ ConsoleCustomerList/src/CustomerStorage.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+
 public class CustomerStorage
 {
     private HashMap<String, Customer> storage;
@@ -8,40 +9,37 @@ public class CustomerStorage
         storage = new HashMap<>();
     }
 
-    public void addCustomer(String data) throws IllegalAccessException {
+    public void addCustomer(String data)
+    {
         String[] components = data.split("\\s+");
+        if(components.length != 4){throw new IllegalArgumentException("Не правильный формат, правильный формат: \n add Василий Петров vasily.petrov@gmail.com +79215637722");}
         String name = components[0] + " " + components[1];
-
-        // Проверка, что команда содержит ровно 4 элемента
-        if (components.length != 4){throw new IllegalArgumentException("КОМАНДА add ДОЛЖНА СОДЕРЖАТЬ 4 ЭЛЕМЕНТА" + Main.commandError);}
-
-        // Проверка, что email заканчивается на "@gmail.com"
-        if(!components[2].endsWith("@gmail.com")){throw new IllegalArgumentException("Email ВВЕДЕН НЕ ВЕРНО " + Main.commandError);}
-
-        // Удаление всех нецифровых символов из строки номера телефона
-        String cleanPhone = components[3].replaceAll("[^0-9]","");
-
-        // Проверка, что очищенный номер телефона содержит ровно 11 цифр
-        if(cleanPhone.length() != 11){throw new IllegalArgumentException("4 ЭЛЕМЕНТ НЕ СОДЕРЖИТ НОМЕРА ИЛИ ОН ВВЕДЕН НЕ КОРРЕКТНО " + Main.commandError);}
-
-        // Создание нового объекта Customer и добавление его в хранилище
+        //Добавляем проверку корректности email
+        if (!components[2].matches(".+@gmail\\.com")) {
+            throw new IllegalArgumentException("Не корректный формат email, правильный формат: vasily.petrov@gmail.com");
+        }
+        if (!components[3].matches("\\+7\\d{10}")) {
+            throw new IllegalArgumentException("Не корректный формат email, правильный формат: vasily.petrov@gmail.com");
+        }
         storage.put(name, new Customer(name, components[3], components[2]));
     }
 
     public void listCustomers()
     {
-        System.out.println("Listing all customers. Current size: " + storage.size());
         storage.values().forEach(System.out::println);
     }
 
     public void removeCustomer(String name)
     {
-        System.out.println("Removing customer with name: " + name);
-        storage.remove(name);
-        System.out.println("Customer removed. Current storage size: " + storage.size());
-    }
-    public boolean customerExists(String name) {
-        return storage.containsKey(name);
+        // Если метод remove возвращает null, это значит, что элемент с ключом name не существует
+        Customer removedCustomer = storage.remove(name);
+        if (removedCustomer == null) {
+            // Генерируем исключение, если клиент с таким именем отсутствует
+            throw new IllegalArgumentException("Клиент с именем \"" + name + "\" не найден в коллекции!");
+        } else {
+            // Уведомляем об успешном удалении
+            System.out.println("Клиент \"" + name + "\" был успешно удален из коллекции.");
+        }
     }
 
     public int getCount()
--- ConsoleCustomerList/src/Main.java
+++ ConsoleCustomerList/src/Main.java
@@ -2,56 +2,45 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static String addCommand = "add Василий Петров " +
+    private static String addCommand = "add Василий Петров " +
             "vasily.petrov@gmail.com +79215637722";
-    public static String commandExamples = "\t" + addCommand + "\n" +
+    private static String commandExamples = "\t" + addCommand + "\n" +
             "\tlist\n\tcount\n\tremove Василий Петров";
-    public static String commandError = "Wrong command! Available command examples: \n" +
+    private static String commandError = "Wrong command! Available command examples: \n" +
             commandExamples;
-    public static String helpText = "Command examples:\n" + commandExamples;
+    private static String helpText = "Command examples:\n" + commandExamples;
 
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
+        //База клиентов
         CustomerStorage executor = new CustomerStorage();
         for(;;)
         {
-            String command = scanner.nextLine();
-            //Число 2 в методе split указывает на максимальное количество частей,
-            // на которые должна быть разбита строка. То есть, строка будет разбита на максимум два токена.
-            String[] tokens = command.split("\\s+", 2);
-            try{
-                if(tokens[0].equals("add")) {
-
-                    if(tokens.length == 1){throw new IllegalAccessException("КОМАНДА add ДОЛЖНА СОДЕРЖАТЬ 4 ЭЛЕМЕНТА " + commandError);}
+            try {
+                String command = scanner.nextLine();
+                String[] tokens = command.split("\\s+", 2);
+                if (tokens[0].equals("add")) {
                     executor.addCustomer(tokens[1]);
-                }
-                else if(tokens[0].equals("list")) {
-                    if(tokens.length != 1){throw new IllegalAccessException("КОМОНДА list НЕ ДОЛЖНА СОДЕРЖАТЬ ДОПОЛНЕНИЯ " + commandError);}
+                } else if (tokens[0].equals("list")) {
                     executor.listCustomers();
-                }
-                else if(tokens[0].equals("remove"))
-                {
-                    if(tokens.length == 1){throw new IllegalAccessException("КОМАНДА remove ДОЛЖНА СОДЕРЖАТЬ ИМЯ ЭЛЕМЕНТА " + commandError);}
-
-                    if(!executor.customerExists(tokens[1])){throw new IllegalArgumentException("ЭЛЕМЕНТ С ТАКИМ ИМЕННЕМ НЕ НАЙДЕН В КОЛЛЕКЦИИ " + commandError);}
+                } else if (tokens[0].equals("remove")) {
                     executor.removeCustomer(tokens[1]);
-
-                }
-                else if(tokens[0].equals("count")) {
-                    if(tokens.length != 1){throw new IllegalAccessException("КОМОНДА count НЕ ДОЛЖНА СОДЕРЖАТЬ ДОПОЛНЕНИЯ " + commandError);}
+                } else if (tokens[0].equals("count")) {
                     System.out.println("There are " + executor.getCount() + " customers");
-                }
-                else if(tokens[0].equals("help")) {
-                    if(tokens.length != 1){throw new IllegalAccessException("КОМОНДА help НЕ ДОЛЖНА СОДЕРЖАТЬ ДОПОЛНЕНИЯ " + commandError);}
+                } else if (tokens[0].equals("help")) {
                     System.out.println(helpText);
-
-                }
-                else {
+                } else {
                     System.out.println(commandError);
                 }
-            }catch(Exception Ex){
-                System.out.println(Ex.getMessage()); }
+            }
+            catch(IllegalArgumentException ex){
+                System.out.println(ex.getMessage());
+            }
+            //обработка других непредвиденных ошибок
+            catch(Exception ex){
+                System.out.println(commandError);
+            }
         }
     }
 }
